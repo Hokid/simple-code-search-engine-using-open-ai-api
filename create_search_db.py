c34e3a1fa50f9931ede747e6d09da4a5347ad041
Kirill Khoroshilov <mr.hokid@gmail.com>
2023-02-10 13:28:33 +0300

    missing __main__ check

--- create_search_db.py
+++ create_search_db.py
@@ -6,11 +6,10 @@ from tenacity import wait_random_exponential, stop_after_attempt
 
 get_embedding = _get_embedding.retry_with(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(10))
 
-result = run(['node', 'code-to-csv.js'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-
-if result.returncode != 0:
-    raise RuntimeError(result.stderr)
-
-db = read_csv(StringIO(result.stdout))
-db['embedding'] = db['combined'].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
-db.to_csv("search_db.csv", index=False)
+if __name__ == '__main__':
+    result = run(['node', 'code-to-csv.js'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    if result.returncode != 0:
+        raise RuntimeError(result.stderr)
+    db = read_csv(StringIO(result.stdout))
+    db['embedding'] = db['combined'].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+    db.to_csv("search_db.csv", index=False)
